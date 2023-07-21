abf1e78e2db933c28a01112116dc77e25920f535
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-21 16:47:00 +0200

    Parse error: syntax error, unexpected token ";", expecting "function" or "const" in C:\MAMP\htdocs\php-oop-1\index.php on line 14

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Movie{
   public $author;
   public $yearOfOriginalPublication;
   public $yearOfPublication;
-  public $allThePublication = getAllYears($yearOfOriginalPublication,$yearOfPublication);
+ 
 
   public function __construct($movieName,$author,$yearOfOriginalPublication,$yearOfPublication){
     $this->movieName = $movieName;
@@ -12,14 +12,14 @@ class Movie{
     $this->yearOfOriginalPublication = $yearOfOriginalPublication;
     $this->yearOfPublication = $yearOfPublication;
   };
-  public function getAllYears($yearOfOriginalPublication,$yearOfPublication)
-  {
-  return $yearOfOriginalPublication + $yearOfPublication;
+  public function getAllYears() {
+    return $this->yearOfOriginalPublication + $this->yearOfPublication;
   };
 
 };
 $movie = new Movie('Quasi bianca neve', 'fratelli  Grimm', '1938' ,'2023');
 $movie2 = new Movie('Cenerentola', 'fratelli  Grimm', '1950' ,'2015');
-var_dump($movie);
-var_dump($movie2);
+var_dump($movie getAllYears());
+var_dump($movie2 getAllYears());
+;
 ?>
