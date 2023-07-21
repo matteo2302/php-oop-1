5c6453f6da4211a197fd092b54b093b73bb8530a
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-07-21 16:21:46 +0200

    get all years breaking evrything

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+class Movie{
+  public $movieName;
+  public $author;
+  public $yearOfOriginalPublication;
+  public $yearOfPublication;
+  public $allThePublication = getAllYears($yearOfOriginalPublication,$yearOfPublication);
+
+  function __construct($movieName,$author,$yearOfOriginalPublication,$yearOfPublication){
+    $this->movieName = $movieName;
+    $this->author = $author;
+    $this->yearOfOriginalPublication = $yearOfOriginalPublication;
+    $this->yearOfPublication = $yearOfPublication;
+  }
+  function getAllYears($yearOfOriginalPublication,$yearOfPublication)
+  {
+  return '$yearOfOriginalPublication + $yearOfPublication';
+  };
+
+};
+$movie = new Movie('Quasi bianca neve', 'fratelli  Grimm', '1938' ,'2023');
+$movie2 = new Movie('Cenerentola', 'fratelli  Grimm', '1950' ,'2015');
+var_dump($movie);
+var_dump($movie2);
+?>
